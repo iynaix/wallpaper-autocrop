7a48e209d607c636cde54f1c18885d4c12350977
Lin Xianyi <iynaix@gmail.com>
2023-10-23 08:42:25 +0800

    Initial flake commit

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = {
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              # https://devenv.sh/reference/options/
+              packages = with pkgs;
+                [
+                  gcc
+                  # cudaPackages.libcusolver
+                  # linuxPackages.nvidia_x11
+                  zlib
+                ]
+                ++ (with python3.pkgs; [
+                  (opencv4.override {
+                    enableGtk3 = true;
+                  })
+                  torch
+                ]);
+
+              dotenv.disableHint = true;
+              languages.python = {
+                enable = true;
+                venv.enable = true;
+              };
+            }
+          ];
+        };
+      });
+  };
+}
--- run_directory.py
+++ run_directory.py
@@ -24,20 +24,16 @@ if __name__ == "__main__":
     parser.add_argument("--confidence-threshold", help="Confidence threshold (default=%(default)s).", type=float, default=None)
     parser.add_argument("--nms-threshold", help="NMS threshold (default=%(default)s).", type=float, default=None)
     parser.add_argument("--extensions", help="Target extensions split by commas (default=%(default)s).", default="jpg,png,jpeg")
-    
+
     args = parser.parse_args()
-    
+
     # Disable MXNET CUDNN autotuning since the images come in different sizes.
     os.environ["MXNET_CUDNN_AUTOTUNE_DEFAULT"] = "0"
 
     with open(args.detector_path, "r") as f:
         config = json.load(f)
-    if args.use_gpu > 0:
-        use_gpu = True
-    else:
-        use_gpu = False
-    detector = LFFDDetector(config, use_gpu=use_gpu)
-    
+    detector = LFFDDetector(config, use_gpu=False)
+
     extensions = args.extensions.split(",")
     paths = list(more_itertools.flatten([glob.glob(os.path.join(args.input_directory, "**", f"*.{ext}"), recursive=True) for ext in extensions]))
     print(f"{time.ctime()}: Start inferencing. Press `q` to cancel. Press  `-` to go back.")
@@ -48,14 +44,14 @@ if __name__ == "__main__":
         start = time.time()
         boxes = detector.detect(image, size=args.size, confidence_threshold=args.confidence_threshold, nms_threshold=args.nms_threshold)
         elapsed = time.time() - start
-        drawn_image = detector.draw(image, boxes)
+        drawn_image = detector.draw(image, boxes, color=(0,0,255), thickness=3)
         drawn_image = imutils.resize(drawn_image, height=args.display_height)
         cv2.imshow("Image", drawn_image)
         print(f"{time.ctime()}: [{idx:>7}] Inference time: {elapsed:.4f} seconds. Path: {path}")
         key = cv2.waitKey(0) & 0xFF
         if key == ord("q"):
             break
-        elif key == ord("-"):
+        elif key == ord("p"):
             idx = max(idx - 1, 0)
         else:
             idx += 1
