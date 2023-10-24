9dd4a83f83e870c60fdc0f43ce63e6f7ac655ed9
Lin Xianyi <iynaix@gmail.com>
2023-10-24 17:01:51 +0800

    Use python3.withPackages for python dependencies

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Crops anime wallpapers for a vertical 16:9 monitor.
 
 Uses face detection, based on:
 https://github.com/cheese-roll/light-anime-face-detector
+
+After devenv sets up the flake, install the python dependencies with `pip install- r requirements.txt`
--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # mxnet breaks with numpy >= 1.24, use numpy 1.23.5
+    # https://github.com/apache/mxnet/pull/21223
+    nixpkgs.url = "github:NixOS/nixpkgs/84e33aea0f7a8375c92458c5b6cad75fa1dd561b";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
   };
@@ -23,23 +25,23 @@
           modules = [
             {
               # https://devenv.sh/reference/options/
-              packages = with pkgs;
-                [
-                  gcc
-                  zlib
-                ]
-                ++ (with python3.pkgs; [
-                  (opencv4.override {
-                    enableGtk3 = true;
-                  })
-                  torch
-                  flake8
-                  black
-                ]);
+              # packages = with pkgs; [
+              #   gcc
+              #   zlib
+              # ];
 
               dotenv.disableHint = true;
               languages.python = {
                 enable = true;
+                package = pkgs.python3.withPackages (ps:
+                  with ps; [
+                    # needed for showing the opencv image preview
+                    (opencv4.override {enableGtk3 = true;})
+                    numpy
+                    torch
+                    flake8
+                    black
+                  ]);
                 venv.enable = true;
               };
             }
